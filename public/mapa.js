f37ab732ddb75a6f3e81c93f564c197fb984e8d7
cristianh <crishernandez08@hotmail.com>
2022-10-19 17:01:07 -0500

    first commit

---
+++ public/mapa.js
@@ -0,0 +1,85 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const appDiv = document.getElementById('app');
+   /*  appDiv.innerHTML = `<h1>Obtener Geolocalización</h1>`; */
+
+    const options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    };
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((pos) => {
+            appDiv.innerHTML += `<p>Latitud: ${pos.coords.latitude}</p>`;
+            appDiv.innerHTML += `<p>Longitud: ${pos.coords.longitude}</p>`;
+            loadMap(parseFloat(pos.coords.latitude), parseFloat(pos.coords.longitude))
+            socket.emit('chat message', {'lat':pos.coords.latitude,'log':pos.coords.longitude});
+        }, (error) => {
+            switch (error.code) {
+                case error.PERMISSION_DENIED:
+                    console.log("User denied the request for Geolocation.")
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    console.log("Location information is unavailable.")
+                    break;
+                case error.TIMEOUT:
+                    console.log("The request to get user location timed out.")
+                    break;
+            }
+        }, options);
+    } else {
+        console.log("Your browser doesn't support geolocation.")
+    }
+
+    // Usando watchPosition()
+    let watchID = navigator.geolocation.watchPosition((pos) => {
+        appDiv.innerHTML += `<h2>WatchPosition( )</h2>`;
+        appDiv.innerHTML += `<p>Latitud: ${pos.coords.latitude}</p>`;
+        appDiv.innerHTML += `<p>Longitud: ${pos.coords.longitude}</p>`;
+        
+
+    });
+
+    navigator.geolocation.clearWatch(watchID);
+
+    const loadMap = (lat, lng) => {
+        console.log(lat,lng);
+
+        mapboxgl.accessToken = 'pk.eyJ1IjoiY3J1c3RvMjAyMiIsImEiOiJjbDg3c3lmaTExNmg4M3BubGhyMThvMmhsIn0.AhcG868gRKbP-zDiccuMdA';
+        const map = new mapboxgl.Map({
+            container: 'map', // container ID
+            style: 'mapbox://styles/mapbox/streets-v11', // style URL
+            center: [lng, lat], // starting position [lng, lat]
+            zoom: 14, // starting zoom
+            projection: 'globe' // display the map as a 3D globe
+        });
+        map.on('style.load', () => {
+            map.setFog({}); // Set the default atmosphere style
+        });
+        // Add zoom and rotation controls to the map.
+        map.addControl(new mapboxgl.NavigationControl());
+
+        //createMarket(lat, lng,this.map)
+        /* const marker = new mapboxgl.Marker()
+        .setLngLat([lng, lat])
+        .addTo(map); */
+
+         socket.on('chat send server message', (msg) => {
+            console.log(msg)
+            const {lat, log} = msg
+            const marker = new mapboxgl.Marker()
+                .setLngLat([log,lat])
+                .addTo(map);
+        });
+    }
+
+    
+
+    const createMarket = (lat, lng,map) => {
+        // Create a new marker.
+        const marker = new mapboxgl.Marker()
+            .setLngLat([lat, lng])
+            .addTo(map);
+    }
+})
+
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
+const server = http.createServer(app);
+
+//socket 
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+// Servimos los archivos que se encuentran en el directorio public
+app.use(express.static(path.join(__dirname, './public')));
+app.get('/', (req, res) => {
+  // Servimos los archivos que se encuentran en el directorio public
+
+
+
+  /* res.sendFile(__dirname + '/index.html'); */
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    //socket.broadcast.emit('hi');
+    io.emit('chat send server message', msg);
+  });
+});
+
+
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
